7a87847b4a309052c4d594aa5cafa3df10a0d47c
VERNIERELoic <loic.verniere@cpe.frm>
2023-06-06 23:41:21 +0200

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
-  app.enableCors({origin : ["http://localhost:3000", "http://localhost:4200"], maxAge : 7200});
+  app.enableCors({origin : [process.env.BACK, process.env.FRONT], maxAge : 7200});
   console.log(`Application is running on: ${await app.getUrl()}`);
  
 }
