257279f4ea542c4a08793d0690146ee6bd55ccda
VERNIERELoic <loic.verniere@cpe.fr>
2023-06-18 21:19:43 +0200

    fix team

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -8,12 +8,14 @@ import { MailingService } from 'src/mailing/mailing.service';
 import { UsersService } from 'src/users/users.service';
 import { TeamsService } from 'src/teams/teams.service';
 import { Teams } from 'src/teams/teams.entity';
+import { userEvent } from 'src/user-event/user-event.entity';
+import { UserEventService } from 'src/user-event/user-event.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Events, User, Teams]),
+    TypeOrmModule.forFeature([Events, User, Teams, userEvent]),
   ],
-  providers: [EventsService, MailingService, UsersService, TeamsService],
+  providers: [EventsService, MailingService, UsersService, TeamsService, UserEventService],
   exports: [EventsService],
   controllers: [EventsController],
 })
